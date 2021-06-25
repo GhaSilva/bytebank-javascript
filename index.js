583fa06811f5f41e6d194ccdb3f91e99f3c39627
GhaSilva <ghabriel-silva@outlook.com>
2021-06-25 11:11:08 -0300

    Importando Módulos

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-class Cliente{
-    nome;
-    cpf;
-}
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-            } 
-        
-        }
-    depositar(valor){
-        if(valor <= 0) return;
-            
-        this._saldo += valor;    
-        }
-    }
-    
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
