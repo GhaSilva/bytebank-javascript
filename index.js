e34edb384064759521c702d401c55af5ab40efdb
GhaSilva <ghabriel-silva@outlook.com>
2021-06-25 10:11:49 -0300

    Adicionando sistema de saque

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 class Cliente{
     nome;
     cpf;
+}
+class ContaCorrente{
     agencia;
     saldo;
-}
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
 
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            }
+        }
+    }
+    
+
+const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const ContaCorrenteRicardo = new ContaCorrente();
+ContaCorrenteRicardo.saldo = 0;
+ContaCorrenteRicardo.agencia = 1001;
+console.log(ContaCorrenteRicardo.saldo);
+ContaCorrenteRicardo.saldo = 100;
+ContaCorrenteRicardo.sacar(50);
+
+
+
+console.log(ContaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
