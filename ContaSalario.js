9a6271dce3ba8cca50980fc5f96bc2bf9fef4468
GhaSilva <ghabriel-silva@outlook.com>
2021-06-29 18:44:19 -0300

    Criação da Classe ContaSalario

---
+++ ContaSalario.js
@@ -0,0 +1,14 @@
+import { Conta } from "./Conta.js";
+
+export class ContaSalario extends Conta{
+    constructor(cliente){
+        super(0, cliente, 100)
+    }
+
+    sacar(valor){
+        let taxa = 1.01  
+        if(this._saldo >= valor ){
+            this._saldo -= valor * taxa;
+            return valor;
+            } 
+}
